6438c8f7c08bfa58c54872d20cb484b4e9df1afc
k1LoW <k1lowxb@gmail.com>
2017-04-18 01:18:47 +0900

    Reopen issue and create comment if same (open/closed) issue exists.

--- src/lib/github.js
+++ src/lib/github.js
@@ -1,31 +1,9 @@
 'use strict';
 
-const timestampFormat = 'YYYY-MM-DDTHH:mm:ssZ'; // ISO8601
-const timeunits = require('./timeunits');
-const yaml = require('js-yaml');
-const fs = require('fs');
-const config = yaml.safeLoad(fs.readFileSync(__dirname + '/../../config.yml', 'utf8'));
-const timeunitFormat = timeunits[config.timeunit];
-const moment = require('moment');
-const storage = require('./storage');
-const bucketName = config.s3BucketName;
 const github = require('github');
 
 module.exports = (n, errorData) => {
-    const byTimeunit = moment(moment(errorData.timestamp).format(timeunitFormat), timeunitFormat).format(timestampFormat);
-    const key = 'projects/'
-              + errorData.project + '/'
-              + errorData.message + '/'
-              + byTimeunit + '/'
-              + errorData.project + '-'
-              + errorData.message + '-'
-              + errorData.timestamp + '.json';
-
-    const params = {
-        Bucket: bucketName,
-        Key: key
-    };
-    const signedUrl = storage.getSignedUrl(params);
+    const title = `[${errorData.type}] ${errorData.message}`;
 
     const g = new github({
         timeout: 5000
@@ -36,43 +14,69 @@ module.exports = (n, errorData) => {
         token: n.userToken
     });
 
-    let issue = {
+    g.issues.getForRepo({
         owner: n.owner,
         repo: n.repo,
-        title: `[${errorData.type}] ${errorData.message}`
-    };
-
-    if (n.labels) {
-        issue.labels = n.labels;
-    }
-
-    let body = '';
-
-    if (errorData.backtrace) {
-        let backtrace = `## backtrace
-
-`;
-        backtrace += "```\n";
-        errorData.backtrace.forEach((b) => {
-            backtrace += b.file + '(' + b.line + ') ' + b.function + "\n";
+        state: 'all',
+        per_page: 100
+    }).then((res) => {
+        const filtered = res.filter((issue) => {
+            return issue.title == title;
         });
-        backtrace += "```\n\n";
-        body += backtrace;
-    }
-
-    ['context', 'environment', 'session', 'params'].forEach((k) => {
-        if (errorData[k] && JSON.stringify(errorData[k], null, 2) != '{}') {
-            let content = '## ' + k;
-            content += "\n\n\n";
-            content += "```\n";
-            content += JSON.stringify(errorData[k], null, 2);
-            content += "\n";
-            content += "```\n\n";
-            body += content;
+        if (filtered.length == 1) {
+            return g.issues.edit({
+                owner: n.owner,
+                repo: n.repo,
+                number: filtered[0].number,
+                state: 'open'
+            });
         }
-    });
+        return true;
+    }).then((res) => {
+        let issue = {
+            owner: n.owner,
+            repo: n.repo
+        };
 
-    let footer = `## project
+        if (n.labels) {
+            issue.labels = n.labels;
+        }
+
+        let body = '';
+
+        if (errorData.backtrace) {
+            let backtrace = '## backtrace\n\n';
+            backtrace += '```\n';
+            errorData.backtrace.forEach((b) => {
+                backtrace += b.file + '(' + b.line + ') ' + b.function + '\n';
+            });
+            backtrace += '```\n\n';
+            body += backtrace;
+        }
+
+        ['context', 'environment', 'session', 'params'].forEach((k) => {
+            if (errorData[k] && JSON.stringify(errorData[k], null, 2) != '{}') {
+                let content = '## ' + k;
+                content += '\n\n\n';
+                content += '```\n';
+                content += JSON.stringify(errorData[k], null, 2);
+                content += '\n';
+                content += '```\n\n';
+                body += content;
+            }
+        });
+
+        if (res.number) {
+            // create comment
+            issue.number = res.number;
+            issue.body = body;
+            return g.issues.createComment(issue);
+        }
+
+        // create issue
+        issue.title = title;
+
+        let footer = `## project
 ${errorData.project}
 
 ## type
@@ -83,16 +87,16 @@ ${errorData.timestamp}
 
 `;
 
-    footer += '> This issue was created by [faultline](https://github.com/k1LoW/faultline).';
-    body += footer;
+        footer += '> This issue was created by [faultline](https://github.com/k1LoW/faultline).';
+        body += footer;
 
-    issue.body = body;
+        issue.body = body;
 
-    g.issues.create(issue)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+        return g.issues.create(issue);
+    }).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
+
 };
