af08f5167babd86eb128601b6dccdddafca91d57
k1LoW <k1lowxb@gmail.com>
2018-01-30 11:33:15 +0900

    Add Invalid API Key test

--- src/handlers/__tests__/errorsPost.js
+++ src/handlers/__tests__/errorsPost.js
@@ -59,4 +59,48 @@ describe('errorsPost.handler', () => {
         };
         handler(event, context, cb);
     });
+
+    it ('When invalid X-Api-Key, response should be 403 error', (done) => {
+        const event = {
+            headers: {
+                'X-Api-Key': 'Invalid'
+            },
+            pathParameters: {
+                project: 'sample-project'
+            },
+            body: JSON.stringify({
+                errors: [
+                    {
+                        type: 'notice',
+                        message: 'Undefined index: faultline',
+                        backtrace: [
+                            {
+                                file: '/var/www/test/test.php',
+                                line: 15,
+                                function: 'SomeClass->__construct()'
+                            },
+                            {
+                                file: '/var/www/test/SomeClass.class.php',
+                                line: 36,
+                                function: 'SomeClass->callSomething()'
+                            }
+                        ],
+                        timestamp: '2016-11-02T00:01:00+00:00'
+                    },
+                ],
+                notifications: []
+            })
+        };
+        const context = {};
+
+        const cb = (error, response) => {
+            return Promise.resolve().then(() => {
+                assert(error === null);
+                assert(response.statusCode === 403);
+                assert(response.headers['Access-Control-Allow-Origin'] === '*');
+                assert(response.body === JSON.stringify({ errors: [{ message: '403 Forbidden' }] }, null, 2));
+            }).then(done, done);
+        };
+        handler(event, context, cb);
+    });
 });
