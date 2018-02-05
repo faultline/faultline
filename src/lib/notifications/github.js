ef805034be7fcc46d64ff5a46fb4fba50c1cf5b0
k1LoW <k1lowxb@gmail.com>
2018-02-05 13:15:10 +0900

    Update github -> @octokit/rest

--- src/lib/notifications/github.js
+++ src/lib/notifications/github.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const console = require('console');
-const github = require('github');
+const octokit = require('@octokit/rest')({
+    timeout: 5000
+});
 const messageBuilder = require('../messageBuilder');
 
 module.exports = (n, errorData) => {
     const title = messageBuilder.title(n, errorData);
-    const g = new github({
-        timeout: 5000
-    });
 
-    g.authenticate({
+    octokit.authenticate({
         type: 'token',
         token: n.userToken
     });
@@ -43,11 +42,11 @@ module.exports = (n, errorData) => {
             per_page: limit,
             page: page
         };
-        return g.issues.getForRepo(condition).then(function(results) {
-            if (!results.length)
+        return octokit.issues.getForRepo(condition).then((res) => {
+            if (!res.data.length)
                 return arr;
             else
-                return getAllIssues(arr.concat(results));
+                return getAllIssues(arr.concat(res.data));
         });
     }
 
@@ -61,11 +60,11 @@ module.exports = (n, errorData) => {
             labels = n.labels;
         }
         const body = messageBuilder.body(n, errorData);
-        if (filtered.length == 1) {
+        if (filtered.length === 1) {
             const number = filtered[0].number;
             if (isReopen()) {
                 if (isUpdate()) {
-                    promises.push(g.issues.edit({
+                    promises.push(octokit.issues.edit({
                         owner: n.owner,
                         repo: n.repo,
                         number: number,
@@ -75,7 +74,7 @@ module.exports = (n, errorData) => {
                         body: body + messageBuilder.footer(n, errorData)
                     }));
                 } else {
-                    promises.push(g.issues.edit({
+                    promises.push(octokit.issues.edit({
                         owner: n.owner,
                         repo: n.repo,
                         number: number,
@@ -84,7 +83,7 @@ module.exports = (n, errorData) => {
                 }
             }
             if (isComment()) {
-                promises.push(g.issues.createComment({
+                promises.push(octokit.issues.createComment({
                     owner: n.owner,
                     repo: n.repo,
                     number: number,
@@ -93,7 +92,7 @@ module.exports = (n, errorData) => {
             }
         } else {
             // Create issue
-            promises.push(g.issues.create({
+            promises.push(octokit.issues.create({
                 owner: n.owner,
                 repo: n.repo,
                 title: title,
