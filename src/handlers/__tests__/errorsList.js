967d77eb226be399482905f45a1a1f9db2134751
k1LoW <k1lowxb@gmail.com>
2018-02-09 00:51:12 +0900

    Add tests

--- src/handlers/__tests__/errorsList.js
+++ src/handlers/__tests__/errorsList.js
@@ -1,11 +1,17 @@
 'use strict';
 const { describe, it, beforeEach, afterEach } = require('mocha');
 const assert = require('power-assert');
+const Ajv = require('ajv');
+const deref = require('json-schema-deref-sync');
+const { rootSchema } = require('../../lib/constants');
+const ajvKeywords = require('ajv-keywords');
+const ajv = new Ajv({ v5: true, $data: true, allErrors: true });
+ajvKeywords(ajv);
+const targetSchema = deref(rootSchema).properties.error.links.find((l) => {
+    return l.rel == 'errors';
+}).targetSchema;
 
-const {
-    mockAws
-} = require('../../lib/mockUtility');
-
+const { mockAws } = require('../../lib/mockUtility');
 const errorsPostHandler = require('./../errorsPost.js').handlerBuilder(mockAws);
 const handler = require('./../errorsList.js').handlerBuilder(mockAws);
 
@@ -75,6 +81,36 @@ describe('errorsList.handler', () => {
                 assert(error === null);
                 assert(response.statusCode === 200);
                 assert(response.headers['Access-Control-Allow-Origin'] === '*');
+                assert(ajv.validate(targetSchema, response.body));
+                const parsed = JSON.parse(response.body);
+                assert(parsed.data.errors.length === 1);
+                resolve();
+            };
+            handler(event, context, cb);
+        });
+    });
+
+    it ('GET `resolved` errors, 0 errors', () => {
+        const event = {
+            httpMethod: 'GET',
+            headers: {
+                'X-Api-Key': process.env.FAULTLINE_MASTER_API_KEY
+            },
+            pathParameters: {
+                project: encodeURIComponent('sample-project'),
+                message: encodeURIComponent('Undefined index: faultline')
+            },
+            queryStringParameters: {
+                status: 'resolved'
+            }
+        };
+        const context = {};
+
+        return new Promise((resolve) => {
+            const cb = (error, response) => {
+                assert(ajv.validate(targetSchema, response.body));
+                const parsed = JSON.parse(response.body);
+                assert(parsed.data.errors.length === 0);
                 resolve();
             };
             handler(event, context, cb);
